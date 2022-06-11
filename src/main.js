8d0aa0b572a2187e0bf0755462c0bf533f135da3
zy-smile <64716566+zy-smile@users.noreply.github.com>
2022-06-11 20:41:39 +0800

    测试web版

--- src/main.js
+++ src/main.js
@@ -36,9 +36,6 @@ Vue.filter('dateFormat', function(originVal) {
     const y = dt.getFullYear()
     const m = (dt.getMonth() + 1 + '').padStart(2, '0')
     const d = (dt.getDate() + '').padStart(2, '0')
-        // const hh = (dt.getHours() + '').padStart(2, '0')
-        // const mm = (dt.getMinutes() + '').padStart(2, '0')
-        // const ss = (dt.getSeconds() + '').padStart(2, '0')
     return `${y}-${m}-${d}`
 })
 new Vue({
